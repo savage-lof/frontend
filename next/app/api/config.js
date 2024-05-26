d7c5ee653666233e497dd411ab8a8de8653fa04d
savage-lof <63022910+savage-lof@users.noreply.github.com>
2024-05-26 15:07:01 +0300

    new front

--- next/app/api/config.js
+++
@@ -1,8 +0,0 @@
-export const BASE_URL = 'https://lula.nomoredomainswork.ru';
-
-export const endpoints = {
-    games: `${BASE_URL}/games`,
-    auth: `${BASE_URL}/auth/local`,
-    me: `${BASE_URL}/users/me`
-
-};
