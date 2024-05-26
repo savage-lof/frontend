69861f77c585cd1b36f197be25c30e3e06d39420
savage-lof <63022910+savage-lof@users.noreply.github.com>
2024-05-26 15:10:38 +0300

    еще new domain

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://lula.nomoredomainswork.ru'
+export const BASE_URL = 'https://kebab.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
