a4b082292290a2fa8b267889e2e80108fb710f1b
savage-lof <63022910+savage-lof@users.noreply.github.com>
2024-05-28 15:56:10 +0300

    Финальное ревью

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://kebab.nomoredomainswork.ru'
+export const BASE_URL = "https://kebab.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
-}
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+};
